4577e2f767d2fe457e98850f3228c9206de9b0d1
Teju134 <92290728+Teju134@users.noreply.github.com>
2021-10-20 12:11:57 +0530

    Add files via upload

---
+++ escape sequences.c
@@ -0,0 +1,10 @@
+/*c program to print details*/
+#include<stdio.h>
+void main()
+{
+	printf("my name is teju\ni m form gollapalem\n");
+	printf("Aditya\tengineering\tcollege\n");
+	printf("i am studying  btech 3rd year\n");
+	printf("civil department\\section\n");
+	printf("\"B\"section");
+}
