6819126e83596c93ae684a3ac75f52bc8198384c
Teju134 <92290728+Teju134@users.noreply.github.com>
2021-10-20 11:38:57 +0530

    Add files via upload

---
+++ my name.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main ()
+{
+	printf("Joshna\tvinaya\tteju");
+	printf("btech\r3rd\ryear\rstudying");
+	printf("teja\\sri");
+	printf("i m from\"gollapalem\"");
+}
+	
---
+++ product two numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int number1,number2,product;
+	
+	//initialization
+	number1 =20;
+	number2 =30;
+	
+	//calculation
+	product=number1*number2;
+	
+	//output
+	printf("%d",product);
+}
