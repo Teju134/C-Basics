3ea6056b5b1d05af6bb4b9845f578850d6c22a18
Teju134 <92290728+Teju134@users.noreply.github.com>
2021-10-20 12:34:52 +0530

    Add files via upload

---
+++ student details.c
@@ -0,0 +1,19 @@
+/*c program to print details of a person*/
+#include<stdio.h>
+void main()
+{
+	// vcariable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// initialization
+	age=20;
+	cgpa=8.4;
+	initial='p';
+	
+	// output
+	printf("age of student is:%d\n",age);
+	printf("cgpa of student is:%f\n",cgpa);
+	printf("initial of student is:%c",initial);
+}
