afb999198b9a716fedf0705307599f7bde9098e6
Teju134 <92290728+Teju134@users.noreply.github.com>
2021-11-15 12:28:55 +0530

    Add files via upload

--- decoding.c
+++ decoding.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main ()
 {
-   int a,b,c;
-   printf("enter three numbers");
-   scanf("%d%d%d",&a,&b,&c);
-   printf("%c%c%c",a+95 ,b+95,c+95);	
+   char a,b,c;
+   printf("enter three character");
+   scanf("%c%c%c",&a,&b,&c);
+   printf("%d %d %d",a-95,b-95,c-95);	
 }
